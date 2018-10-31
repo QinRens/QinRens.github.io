d14faa57b883750f7ec0f9cdb7b148f39015eff7
YDUP <zhangyadong@stu.xjtu.edu.cn>
2018-10-31 15:34:20 +0800

    basic project

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php
+
+$to = 'ydup@foxmail.com'; // replace this mail with yours
+$name = $_POST["name"];
+$email= $_POST["email"];
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+$message ='<table style="width:100%">
+    <tbody><tr>
+        <td>'.$name.'</td>
+    </tr>
+    <tr><td>Email: '.$email.'</td></tr>
+</tbody></table>';
+ 
+if (@mail($to, $email, $message, $headers))
+{
+    echo 'The message has been sent.';
+}else{
+    echo 'failed';
+}
+?>
